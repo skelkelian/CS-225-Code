dece7cdd3ac61f33739b02c333151a64bd2a043a
skelkelian <skelkelian88@gmail.com>
2022-01-18 15:22:31 -0800

    Initialized repository and create Hello World

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include "lab_1.h"
+#include "lab 2.h"
+#include "lab 3.h"
+#include "input and output files.h"
+#include "static cast.h"
+#include "lab 4.h"
+#include "lab 5.h"
+#include "lab 6.h"
+#include "lab 7.h"
+#include "lab 8.h"
+#include "lab 9.h"
+#include "lab 10.h"
+#include "lab 11.h"
+#include "lab 12.h"
+#include "lab 14.h"
+#include "lab 15.h"
+#include "lab 16.h"
+#include "lab 17.h"
+#include "lab 18.h"
+#include "lab 19.h"
+#include "lab 20.h"
+#include "lab 21.h"
+
+using namespace std;
+
+int main() {
+//    lab1();
+//    lab2();
+//    lab3();
+//    inputOutputFiles();
+//    staticCast();
+//    lab4();
+//    lab5();
+//    lab6();
+//    lab7();
+//    lab8();
+//    lab9();
+//    lab10();
+//    lab11();
+//    lab12();
+//    lab14();
+//    lab15();
+//    lab16();
+//    lab17();
+//    lab18();
+//    lab19();
+//    lab20();
+//    lab21();
+    enum courses {ALGEBRA, BASIC, PASCAL, PHILOSOPHY, ANALYSIS};
+    courses registered;
+    registered = ALGEBRA;
+    cout << registered << endl;
+}
