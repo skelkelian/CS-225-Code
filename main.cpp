4d6763621d8bd10697ac9ec0c67ee708165becac
skelkelian <skelkelian88@gmail.com>
2022-01-25 16:07:58 -0800

    Current main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,26 +1,28 @@
 #include <iostream>
 #include "lab_1.h"
-#include "lab 2.h"
-#include "lab 3.h"
-#include "input and output files.h"
+#include "lab_2.h"
+#include "lab_3.h"
+#include "input_and_output_files.h"
 #include "static cast.h"
-#include "lab 4.h"
-#include "lab 5.h"
-#include "lab 6.h"
-#include "lab 7.h"
-#include "lab 8.h"
-#include "lab 9.h"
-#include "lab 10.h"
-#include "lab 11.h"
-#include "lab 12.h"
-#include "lab 14.h"
-#include "lab 15.h"
-#include "lab 16.h"
-#include "lab 17.h"
-#include "lab 18.h"
-#include "lab 19.h"
-#include "lab 20.h"
-#include "lab 21.h"
+#include "lab_4.h"
+#include "lab_5.h"
+#include "lab_6.h"
+#include "lab_7.h"
+#include "lab_8.h"
+//#include "lab_9.h"
+#include "lab_10.h"
+#include "lab_11.h"
+#include "lab_12.h"
+#include "lab_14.h"
+#include "lab_15.h"
+#include "lab_16.h"
+#include "lab_17.h"
+#include "lab_18.h"
+#include "lab_19.h"
+#include "lab_20.h"
+#include "lab_21.h"
+#include "lab_22.h"
+#include "lab_23.h"
 
 using namespace std;
 
@@ -42,13 +44,11 @@ int main() {
 //    lab14();
 //    lab15();
 //    lab16();
-//    lab17();
+    lab17();
 //    lab18();
 //    lab19();
 //    lab20();
 //    lab21();
-    enum courses {ALGEBRA, BASIC, PASCAL, PHILOSOPHY, ANALYSIS};
-    courses registered;
-    registered = ALGEBRA;
-    cout << registered << endl;
+//    lab22();
+//    lab23();
 }
